4303da4f21ac0a2f04b08b2a6870f1cae8da1ea8
sjha2048 <sjha200000@gmail.com>
2020-11-18 21:44:51 +0530

    added user methods

---
+++ controllers/user_controller.js
@@ -0,0 +1,11 @@
+const models = require("../models");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.signUp = (req, res) => {};
+
+exports.login = (req, res) => {};
+
+exports.deleteUser = (req, res) => {};
+
+exports.attendEvent = (req, res) => {};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const user_controller = require("../controllers/user_controller");
 
+router.post("/signup", user_controller.signUp);
+router.post("/login", user_controller.login);
+router.delete("/deleteUser", user_controller.deleteUser);
+router.patch("/attendEvent", user_controller.attendEvent);
 
-
-
-
-
-module.exports = router
+module.exports = router;
